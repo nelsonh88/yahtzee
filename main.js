ce74ff606c738d7c84b5be4bde3d78676833b625
nelsonh88 <nelson.huezo88@gmail.com>
2019-09-02 20:34:37 -0400

    Worked on the uppersection scoring

--- main.js
+++ main.js
@@ -1,89 +1,108 @@
 // I am declaring variables
+// I decided to go with an object instead of declaring variables seemed to be easier to adjust game scores
+let upperSectionNumbers = {
+    aces: 0,
+    twos: 0,
+    threes: 0,
+    fours: 0,
+    fives: 0,
+    sixes: 0
+},
+    upperSectionScoring = 0,
+    // ^^^ declaring variables for top half of the scoring 
 
-let aces = 0, 
-twos = 0,
-threes = 0,
-fours = 0,
-fives = 0,
-sixes = 0,
-upperSectionScoring = 0, 
-// ^^^ declaring variables for top half of the scoring 
-threeOfAKind = 0,
-fourOfAKind = 0,
-fullHouse = false,
-smallStraight = false,
-largeStraight = false,
-chance = 0,
-yahtzee01 = false,
-yahtzee02 = false,
-yahtzee03 = false,
-lowerSectionScoring = 0,
-// ^^^ declaring variables for bottom half of the scoring
-totalScore = 0;
+    threeOfAKind = 0,
+    fourOfAKind = 0,
+    fullHouse = false,
+    smallStraight = false,
+    largeStraight = false,
+    chance = 0,
+    yahtzee01 = false,
+    yahtzee02 = false,
+    yahtzee03 = false,
+    lowerSectionScoring = 0,
+    // ^^^ declaring variables for bottom half of the scoring
+
+    totalScore = 0;
 // ^^^ declaring vaiable for the entire game score 
 
 // declaring a variable for the upper form so I can iterate through it
-let upperForm = document.getElementById("upper-section_scoring").elements;
+// let upperForm = document.getElementById("upper-section_scoring").elements;
 
 
 // uppersection scoring  
-const checkUpperSectionScoring = function  () {
-    for (let i = 0; i < upperForm.length; i++) {
-        let numberValues = upperForm[i].id
-        // console.log('this is the property in obj', Object.keys(objOfNumVariable))
-        if (numberValues === 'aces') {
-            aces = document.getElementById(numberValues).value;
-            console.log(aces)
-        } else if (numberValues === 'twos') {
-            twos = document.getElementById(numberValues).value;
-        } else if (numberValues === 'threes') {
-            threes = document.getElementById(numberValues).value;
-        } else if (numberValues === 'fours') {
-            fours = document.getElementById(numberValues).value;
-        } else if (numberValues === 'fives') {
-            fives = document.getElementById(numberValues).value;
-        } else if (numberValues === 'sixes') {
-            sixes = document.getElementById(numberValues).value;
-        };
-    }
-  addUpperSectionScoring();
-};
-// checkUpperSectionScoring();
 
-// console.log(aces)
-// console.log(twos)
-// console.log(threes)
-// console.log(fours)
-// console.log(fives)
-// console.log(sixes)
+const checkUpperSectionScoring = function () {
+    // right here I am iterating through the uppertSectionNumbers object to check if it has the same properties as the id in the form
+    for (numberValues in upperSectionNumbers) {
+        if (Object.prototype.hasOwnProperty.call(upperSectionNumbers, numberValues)) {
+            upperSectionNumbers[numberValues] = document.getElementById(numberValues).value;
+            console.log(Object.prototype.hasOwnProperty.call(upperSectionNumbers, numberValues))
+            console.log(numberValues)
+        }
+        addUpperSectionScoring();
+    }
+}
+// const checkUpperSectionScoring = function  () {
+//     for (let i = 0; i < upperForm.length; i++) {
+//         let numberValues = upperForm[i].id
+//         // console.log('this is the property in obj', Object.keys(objOfNumVariable))
+//         if (numberValues === 'aces') {
+//             aces = document.getElementById(numberValues).value;
+//             console.log(aces)
+//         } else if (numberValues === 'twos') {
+//             twos = document.getElementById(numberValues).value;
+//         } else if (numberValues === 'threes') {
+//             threes = document.getElementById(numberValues).value;
+//         } else if (numberValues === 'fours') {
+//             fours = document.getElementById(numberValues).value;
+//         } else if (numberValues === 'fives') {
+//             fives = document.getElementById(numberValues).value;
+//         } else if (numberValues === 'sixes') {
+//             sixes = document.getElementById(numberValues).value;
+//         };
+//     }
+//   addUpperSectionScoring();
+// };
 
 // fn is checking for bonus on the uppersection scorring 
 // if your total score for the upper sction >= 63 then you get the bonus
 const checkForBonus = function () {
-    console.log('this is the upper scroing', upperSectionScoring);
     if (upperSectionScoring >= 63) {
         console.log('bonus!');
         upperSectionScoring = upperSectionScoring + 35;
-        console.log(upperSectionScoring);
-        upperSectionScoring = upperSectionScoring;
         return upperSectionScoring;
     } else {
         console.log('no bonus');
-        upperSectionScoring = upperSectionScoring;
-        return upperSectionScoring;
     }
 };
 
+// const addUpperSectionScoring = function () {
+//     upperSectionScoring = parseInt(aces) + parseInt(twos) + parseInt(threes) + parseInt(fours) + parseInt(fives) + parseInt(sixes);
+
+//     console.log(upperSectionScoring);
+
+//     checkForBonus();
+// }
+
 const addUpperSectionScoring = function () {
-    upperSectionScoring = parseInt(aces) + parseInt(twos) + parseInt(threes) + parseInt(fours) + parseInt(fives) + parseInt(sixes);
-
-    console.log(upperSectionScoring);
-
+    // making a new array to store the values from the original array into number data type
+    let arr2 = []
+    // declaring a new variable that equals Object.values a method that brings back an array of the values in the key value pairs from the object upperSectionNumbers
+    arr1 = Object.values(upperSectionNumbers)
+    // looping through arr1 to see convert each element in the array into a number data type and then pushing the new value into the empty arr2 array
+    for (let i = 0; i < arr1.length; i++) {
+        let newValue = parseInt(arr1[i])
+        arr2.push(newValue)
+    }
+    // using the reduce method to add the values of the array arr2 together which is the total score of the upper section without the bonus
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    upperSectionScoring = arr2.reduce(reducer)
+    // calling the checkForBonus fn to check for the bonus
     checkForBonus();
+
 }
 
-// checkForBonus();
-// console.log('this is the new upper section score', upperSectionScoring);
 
 // lowersection scoring
 
@@ -100,7 +119,7 @@ const checkThreeOfAKind = function () {
 //     } else {
 //         console.log('No three of a kind')
 //     }
-    
+
 // }
 
 const checkFourOfAKind = function () {
@@ -180,17 +199,12 @@ const checkLowerSectionScoring = function () {
 }
 
 
-console.log(totalScore);
+// console.log(totalScore);
 
 const checkTotalScore = function () {
     totalScore = upperSectionScoring + lowerSectionScoring
     return totalScore
-}; 
+};
 
-checkTotalScore();
-console.log(totalScore);
-
-// get elements from html form and set them as variables
-
-console.log(aces)
-console.log(upperSectionScoring)
+// checkTotalScore();
+// console.log(totalScore);
